63fc73856f20c7eaa3ba72b2ce63c32f2322b53c
metacode22 <alohajune22@gmail.com>
2024-01-20 23:18:41 +0900

    feat: useFunnel 구현

---
+++ src/hooks/common/useFunnel.tsx
@@ -0,0 +1,89 @@
+import {
+  Children,
+  isValidElement,
+  ReactElement,
+  ReactNode,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+
+import { assert } from '@/utils/assert';
+
+type NonEmptyArray<T> = readonly [T, ...T[]];
+
+export function useFunnel<Steps extends NonEmptyArray<string>>({
+  steps,
+  initialStep = steps[0],
+  tracingExternalStep = false,
+  externalStep,
+}: {
+  steps: Steps;
+  initialStep?: Steps[number];
+  tracingExternalStep?: boolean;
+  externalStep?: Steps[number];
+}) {
+  const [step, setStep] = useState<Steps[number]>(initialStep);
+
+  const FunnelComponent = useMemo(
+    () =>
+      Object.assign(
+        function _Funnel(props: Omit<FunnelProps<Steps>, 'step'>) {
+          return <Funnel<Steps> step={step} {...props} />;
+        },
+        {
+          Step: function _Step(props: StepProps<Steps>) {
+            return <Step<Steps> {...props} />;
+          },
+        },
+      ),
+    [step],
+  );
+
+  useEffect(() => {
+    if (tracingExternalStep && externalStep) {
+      assert(steps.includes(externalStep), '올바른 externalStep이 아닙니다.');
+      setStep(externalStep ?? initialStep);
+    }
+  }, [tracingExternalStep, externalStep, steps, initialStep]);
+
+  return [FunnelComponent, setStep] as const;
+}
+
+type FunnelProps<Steps extends NonEmptyArray<string>> = {
+  step: Steps[number];
+  children:
+    | Array<ReactElement<StepProps<Steps>>>
+    | ReactElement<StepProps<Steps>>;
+};
+
+function Funnel<Steps extends NonEmptyArray<string>>({
+  step,
+  children,
+}: FunnelProps<Steps>) {
+  const validElements = Children.toArray(children).filter(isValidElement);
+  const targetStepElement = validElements.find(
+    (element) => (element.props as StepProps<Steps>).name === step,
+  );
+
+  if (!targetStepElement) return null;
+
+  return <>{targetStepElement}</>;
+}
+
+type StepProps<Steps extends NonEmptyArray<string>> = {
+  name: Steps[number];
+  onEnter?: () => void;
+  children: ReactNode;
+};
+
+function Step<Steps extends NonEmptyArray<string>>({
+  onEnter,
+  children,
+}: StepProps<Steps>) {
+  useEffect(() => {
+    onEnter?.();
+  }, [onEnter]);
+
+  return <>{children}</>;
+}
