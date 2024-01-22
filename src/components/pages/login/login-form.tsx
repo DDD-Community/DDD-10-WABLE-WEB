96022e6b90b4b76d86d0bd2d17936ada4e349043
조성원 <tjddnjsjo@kakao.com>
2024-01-22 17:35:35 +0900

    feat: react-hook-form 연결

--- src/components/pages/login/login-form.tsx
+++ src/components/pages/login/login-form.tsx
@@ -3,39 +3,65 @@ import {
   Flex,
   FormControl,
   FormErrorMessage,
-  FormHelperText,
   FormLabel,
   Input,
   Spacer,
   Switch,
 } from '@chakra-ui/react';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { Form } from './styles';
+
+interface IFormValues {
+  email: string;
+  password: string;
+}
 
 export function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormValues>();
+
+  const onSubmit: SubmitHandler<IFormValues> = (data) => {
+    alert(JSON.stringify(data));
+  };
+
   return (
-    <>
-      <FormControl>
+    <Form autoComplete="off" onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={!!errors.email}>
         <Flex align="center">
           <FormLabel>이메일</FormLabel>
-          <FormHelperText mt={0} marginInlineEnd={3} marginBottom={2}>
-            *helper text
-          </FormHelperText>
           <FormErrorMessage mt={0} marginInlineEnd={3} marginBottom={2}>
-            *error message
+            {errors.email?.message}
           </FormErrorMessage>
         </Flex>
-        <Input type="email" placeholder="mail@simmmple.com" size="lg" />
+        <Input
+          type="email"
+          placeholder="mail@simmmple.com"
+          size="lg"
+          {...register('email', {
+            required: '이메일을 입력해 주세요.',
+            pattern: {
+              value: /\S+@\S+\.\S+/,
+              message: '이메일 형식이 올바르지 않습니다.',
+            },
+          })}
+        />
       </FormControl>
-      <FormControl>
+      <FormControl isInvalid={!!errors.password}>
         <Flex align="center">
           <FormLabel>비밀번호</FormLabel>
-          <FormHelperText mt={0} marginInlineEnd={3} marginBottom={2}>
-            *helper text
-          </FormHelperText>
           <FormErrorMessage mt={0} marginInlineEnd={3} marginBottom={2}>
-            *error message
+            {errors.password?.message}
           </FormErrorMessage>
         </Flex>
-        <Input type="password" placeholder="Min. 8 characters" size="lg" />
+        <Input
+          type="password"
+          placeholder="Min. 8 characters"
+          size="lg"
+          {...register('password', { required: '비밀번호를 입력해 주세요.' })}
+        />
       </FormControl>
       <Flex align="center">
         <Switch id="save-login-info" mr={3} />
@@ -45,9 +71,9 @@ export function LoginForm() {
         <Spacer />
         <Button variant="link">비밀번호를 잊으셨나요?</Button>
       </Flex>
-      <Button colorScheme="blackAlpha" size="lg" width="full">
+      <Button type="submit" colorScheme="blackAlpha" size="lg" width="full">
         로그인
       </Button>
-    </>
+    </Form>
   );
 }
