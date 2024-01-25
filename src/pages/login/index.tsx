647e77f5384b9f8333c26b0a75aa8ae712133446
dokyung-kim <dkkim0122@gmail.com>
2024-01-25 23:24:51 +0900

    feat: 로그인 구글 svg svgr 사용하여 react 컴포넌트처럼 사용할 수 있도록 변경

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,8 +1,7 @@
 import { Box, Button, Center, Flex, Heading } from '@chakra-ui/react';
-
-import { Icons } from '@/components/common/icons';
 import { DividerWithContent } from '@/components/common/divider-with-content';
 import { LoginForm } from '@/components/pages/login/login-form';
+import Google from '@/assets/icons/google.svg';
 
 export default function Login() {
   return (
@@ -12,7 +11,7 @@ export default function Login() {
           <Heading size="lg" mb={8}>
             로그인
           </Heading>
-          <Button leftIcon={<Icons.google />} size="lg" width="full">
+          <Button leftIcon={<Google />} size="lg" width="full">
             구글 계정으로 로그인
           </Button>
           <DividerWithContent content="or" />
