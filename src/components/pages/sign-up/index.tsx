70b62557ad3e5e1cb0419c4797878e65d7cd6757
dokyung-kim <dkkim0122@gmail.com>
2024-01-29 00:00:00 +0900

    feat: 회원가입 페이지 레이아웃 조정 및 디자인 적용

--- src/components/pages/sign-up/index.tsx
+++ src/components/pages/sign-up/index.tsx
@@ -1,13 +1,28 @@
-import { Heading, VStack } from '@chakra-ui/react';
+import Image from 'next/image';
+import { Heading, VStack, Button } from '@chakra-ui/react';
 import SignUpForm from '@/components/pages/sign-up/sign-up-form';
-import { Container, Section } from './styles';
+import { Container, Background, Section, ArrowIconWrapper } from './styles';
+import SignUpBackgroundImage from '@/assets/images/sign-up-background.png';
+import Google from '@/assets/icons/google.svg';
+import ArrowBack from '@/assets/icons/arrow-back.svg';
+import { DividerWithContent } from '@/components/common/divider-with-content';
 
 export default function SignUp() {
   return (
     <Container>
+      <Background>
+        <Image src={SignUpBackgroundImage} alt="background" />
+      </Background>
       <Section>
-        <VStack spacing="32px">
-          <Heading size="lg">회원가입</Heading>
+        <ArrowIconWrapper>
+          <ArrowBack />
+        </ArrowIconWrapper>
+        <Heading size="lg">회원가입</Heading>
+        <VStack spacing="25px" width="full">
+          <Button leftIcon={<Google />} size="lg" width="full">
+            구글 계정으로 회원가입
+          </Button>
+          <DividerWithContent content="or" />
           <SignUpForm />
         </VStack>
       </Section>
