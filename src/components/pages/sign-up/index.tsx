bb1bb403c1510fb2ed855ae46850c882b686bf4e
dokyung-kim <dkkim0122@gmail.com>
2024-01-31 22:53:15 +0900

    refactor: svg icon import 시 icon suffix를 붙여 구분하도록 함

--- src/components/pages/sign-up/index.tsx
+++ src/components/pages/sign-up/index.tsx
@@ -9,8 +9,8 @@ import {
   ArrowIconWrapper,
 } from './styles';
 import SignUpBackgroundImage from '@/assets/images/sign-up-background.png';
-import Google from '@/assets/icons/google.svg';
-import ArrowBack from '@/assets/icons/arrow-back.svg';
+import GoogleIcon from '@/assets/icons/google.svg';
+import ArrowBackIcon from '@/assets/icons/arrow-back.svg';
 import { DividerWithContent } from '@/components/common/divider-with-content';
 
 export default function SignUp() {
@@ -21,11 +21,11 @@ export default function SignUp() {
       </SignUpBackground>
       <SignUpCardSection>
         <ArrowIconWrapper>
-          <ArrowBack />
+          <ArrowBackIcon />
         </ArrowIconWrapper>
         <Heading size="lg">회원가입</Heading>
         <VStack spacing="25px" width="full">
-          <Button leftIcon={<Google />} size="lg" width="full">
+          <Button leftIcon={<GoogleIcon />} size="lg" width="full">
             구글 계정으로 회원가입
           </Button>
           <DividerWithContent content="or" />
