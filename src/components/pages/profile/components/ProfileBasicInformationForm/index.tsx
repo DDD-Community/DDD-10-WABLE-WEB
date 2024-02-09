2e0483a557095d616ed337e8dc78bacea5e89294
metacode22 <alohajune22@gmail.com>
2024-02-09 17:07:27 +0900

    feat: 생일 입력 받는 input들 렌더링

--- src/components/pages/profile/components/ProfileBasicInformationForm/index.tsx
+++ src/components/pages/profile/components/ProfileBasicInformationForm/index.tsx
@@ -1,16 +1,34 @@
-import { ProfileSteps } from '@/pages/profile/create';
 import {
   Box,
   Button,
   Flex,
   FormControl,
   FormLabel,
+  HStack,
+  Heading,
   Input,
+  InputGroup,
+  InputRightElement,
+  Select,
+  Text,
+  UseRadioProps,
+  useRadio,
+  useRadioGroup,
 } from '@chakra-ui/react';
+import { ReactNode } from 'react';
 import { useFormContext } from 'react-hook-form';
+import { ChevronDownIcon } from '@chakra-ui/icons';
+
+const GENDER_OPTIONS = ['남성', '여성'];
 
 export function ProfileBasicInformationForm() {
   const { register } = useFormContext();
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'gender',
+    defaultValue: '남성',
+    onChange: console.log,
+  });
+  const group = getRootProps();
 
   return (
     <Flex w="100%" gap="32px" flexDirection="column" alignItems="center">
@@ -20,46 +38,117 @@ export function ProfileBasicInformationForm() {
         padding="24px"
         borderRadius="16px"
         justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+        gap="24px"
       >
-        <Flex maxW="412px" w="100%" flexDirection="column" gap="20px">
+        <Flex maxW="412px" w="100%" flexDirection="column" gap="16px">
           <Box>이미지 업로드</Box>
           <FormControl>
-            <FormLabel htmlFor="name">이름*</FormLabel>
+            <FormLabel htmlFor="name">
+              <Heading size="sm">이름*</Heading>
+            </FormLabel>
             <Input
               id="name"
+              size="lg"
               placeholder="이름을 입력해주세요."
               {...register('name')}
             />
           </FormControl>
           <FormControl>
-            <FormLabel htmlFor="name">닉네임</FormLabel>
+            <FormLabel htmlFor="nickname">
+              <Heading size="sm">닉네임</Heading>
+            </FormLabel>
             <Input
-              id="name"
+              id="nickname"
+              size="lg"
               placeholder="이름을 입력해주세요."
               {...register('name')}
             />
           </FormControl>
           <FormControl>
-            <FormLabel htmlFor="name">성별</FormLabel>
-            <Input
-              id="name"
-              placeholder="이름을 입력해주세요."
-              {...register('name')}
-            />
+            <FormLabel htmlFor="gender">
+              <Heading size="sm">성별*</Heading>
+            </FormLabel>
+            <HStack {...group} gap="18px">
+              {GENDER_OPTIONS.map((option) => {
+                const radio = getRadioProps({ value: option });
+
+                return (
+                  <Radio key={option} {...radio}>
+                    {option}
+                  </Radio>
+                );
+              })}
+            </HStack>
           </FormControl>
           <FormControl>
-            <FormLabel htmlFor="name">생일</FormLabel>
-            <Input
-              id="name"
-              placeholder="이름을 입력해주세요."
-              {...register('name')}
-            />
+            <FormLabel htmlFor="birthday">
+              <Heading size="sm">생일*</Heading>
+            </FormLabel>
+            <Flex gap="32px">
+              <Flex gap="8px" flex="1.25" alignItems="flex-end">
+                <Select icon={<ChevronDownIcon />}>
+                  <option>2021</option>
+                </Select>
+                <FormLabel m="0" color="gray.400">
+                  <Text>년</Text>
+                </FormLabel>
+              </Flex>
+              <Flex gap="8px" flex="1" alignItems="flex-end">
+                <Select icon={<ChevronDownIcon />}>
+                  {Array.from({ length: 12 }, (_, i) => i + 1).map((month) => (
+                    <option key={month}>{month}</option>
+                  ))}
+                </Select>
+                <FormLabel m="0" color="gray.400">
+                  <Text>월</Text>
+                </FormLabel>
+              </Flex>
+              <Flex gap="8px" flex="1" alignItems="flex-end">
+                <Select icon={<ChevronDownIcon />}>
+                  {Array.from({ length: 31 }, (_, i) => i + 1).map((day) => (
+                    <option key={day}>{day}</option>
+                  ))}
+                </Select>
+                <FormLabel m="0" color="gray.400">
+                  <Text>일</Text>
+                </FormLabel>
+              </Flex>
+            </Flex>
           </FormControl>
-          <Button type="submit" variant="primary">
-            다음으로
-          </Button>
         </Flex>
+        <Button type="submit" variant="primary" maxW="412px" w="100%" h="48px">
+          다음으로
+        </Button>
       </Flex>
     </Flex>
+  );
+}
+
+function Radio(props: UseRadioProps & { children: ReactNode }) {
+  const { getInputProps, getRadioProps } = useRadio(props);
+  const input = getInputProps();
+  const radio = getRadioProps();
+
+  return (
+    <Box as="label" flex="1 0 0">
+      <input {...input} />
+      <Flex
+        {...radio}
+        cursor="pointer"
+        alignItems="center"
+        justifyContent="center"
+        borderRadius="6px"
+        paddingY="16px"
+        bg="gray.100"
+        _checked={{
+          bg: 'black',
+          color: 'white',
+        }}
+      >
+        <Heading size="sm">{props.children}</Heading>
+      </Flex>
+    </Box>
   );
 }
