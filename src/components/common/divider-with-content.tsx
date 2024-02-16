1d28f10f44f3c2e63222168be0e83112713411c2
조성원 <tjddnjsjo@kakao.com>
2024-02-16 20:21:59 +0900

    feat: DividerWithContent 컴포넌트 bg, fontSize props 추가

--- src/components/common/divider-with-content.tsx
+++ src/components/common/divider-with-content.tsx
@@ -1,10 +1,18 @@
 import { AbsoluteCenter, Box, Divider } from '@chakra-ui/react';
 
-export function DividerWithContent({ content }: { content: React.ReactNode }) {
+export function DividerWithContent({
+  content,
+  bg = 'white',
+  fontSize,
+}: {
+  content: React.ReactNode;
+  bg?: string;
+  fontSize?: number;
+}) {
   return (
     <Box position="relative" paddingY={3} width="full">
       <Divider />
-      <AbsoluteCenter bg="white" px={4}>
+      <AbsoluteCenter bg={bg} px={4} fontSize={fontSize}>
         {content}
       </AbsoluteCenter>
     </Box>
