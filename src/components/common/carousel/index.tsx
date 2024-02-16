4fbb930be984bb4646e930d8b7bfee5bf332fb15
조성원 <tjddnjsjo@kakao.com>
2024-02-17 05:02:42 +0900

    feat: carousel 컴포넌트 제작

---
+++ src/components/common/carousel/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { EmblaOptionsType } from 'embla-carousel';
+import useEmblaCarousel from 'embla-carousel-react';
+import { DotButton, useDotButton } from './carousel-dot-button';
+import {
+  PrevButton,
+  NextButton,
+  usePrevNextButtons,
+} from './carousel-arrow-buttons';
+
+type PropType = {
+  children: React.ReactNode;
+  options?: EmblaOptionsType;
+};
+
+const Carousel: React.FC<PropType> = (props) => {
+  const { children, options } = props;
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi);
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick,
+  } = usePrevNextButtons(emblaApi);
+
+  return (
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">{children}</div>
+      </div>
+
+      <div className="embla__buttons">
+        <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+        <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+      </div>
+
+      <div className="embla__dots">
+        {scrollSnaps.map((_, index) => (
+          <DotButton
+            key={index}
+            onClick={() => onDotButtonClick(index)}
+            className={'embla__dot'.concat(
+              index === selectedIndex ? ' embla__dot--selected' : '',
+            )}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+const Slide: React.FC<PropType> = (props) => {
+  const { children } = props;
+
+  return <div className="embla__slide">{children}</div>;
+};
+
+export { Carousel, Slide };
