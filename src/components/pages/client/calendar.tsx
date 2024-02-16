f75cd57562e3808246d80de36f969c39e4eb9074
조성원 <tjddnjsjo@kakao.com>
2024-02-16 21:28:47 +0900

    feat: react-daypicker custom

--- src/components/pages/client/calendar.tsx
+++ src/components/pages/client/calendar.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { useState } from 'react';
+
 import { DayPicker } from 'react-day-picker';
+import { StyleWrapper } from './calendar.style';
 import 'react-day-picker/dist/style.css';
 
 export default function Calendar() {
@@ -10,15 +12,20 @@ export default function Calendar() {
   );
 
   return (
-    <DayPicker
-      mode="single"
-      captionLayout="dropdown"
-      fromYear={2024}
-      toYear={2024}
-      showOutsideDays
-      required
-      selected={selectedDate}
-      onSelect={setSelectedDate}
-    />
+    <StyleWrapper>
+      <DayPicker
+        mode="single"
+        captionLayout="dropdown"
+        fromYear={2024}
+        toYear={2024}
+        showOutsideDays
+        required
+        selected={selectedDate}
+        modifiers={{ booked: [new Date(2024, 1, 11)] }}
+        modifiersStyles={{ booked: { border: '2px solid currentColor' } }}
+        weekStartsOn={1}
+        onSelect={setSelectedDate}
+      />
+    </StyleWrapper>
   );
 }
