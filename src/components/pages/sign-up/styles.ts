d5e82640f2d41850b0145356bf69818655d2b9c9
조성원 <tjddnjsjo@kakao.com>
2024-02-16 21:29:47 +0900

    style: run stylelint

--- src/components/pages/sign-up/styles.ts
+++ src/components/pages/sign-up/styles.ts
@@ -2,36 +2,39 @@ import styled from '@emotion/styled';
 
 const SignUpContainer = styled.div`
   display: flex;
-  justify-content: center;
   align-items: center;
+  justify-content: center;
   height: 100vh;
 `;
 
 const SignUpBackground = styled.section`
-  padding: 24px;
-  height: 100vh;
-  width: 1440px;
   flex-shrink: 0;
+  width: 1440px;
+  height: 100vh;
+  padding: 24px;
 `;
 
 const SignUpCardSection = styled.section`
   position: absolute;
+
   display: flex;
   flex-direction: column;
   gap: 40px;
   align-items: center;
+
   width: 458px;
   padding: 60px 24px 24px;
+
   background-color: white;
   border-radius: 16px;
   box-shadow: 0 7px 23px 0 #0000000d;
 `;
 
 const ArrowIconWrapper = styled.div`
+  cursor: pointer;
   position: absolute;
   top: 22px;
   left: 22px;
-  cursor: pointer;
 `;
 
 const Form = styled.form`
