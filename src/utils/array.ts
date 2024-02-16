966147e4e9f9811873e86125f6f1b1a847676dbd
조성원 <tjddnjsjo@kakao.com>
2024-02-17 05:23:33 +0900

    feat(utils/array): chunk 함수 추가

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -1,5 +1,17 @@
 type Writeable<T> = { -readonly [P in keyof T]: T[P] };
 
-export function isLastIndex<T>(index: number, array: T[]) {
+function isLastIndex<T>(index: number, array: T[]) {
   return Array.isArray(array) && index === array.length - 1;
 }
+
+function chunk(array: any[], size: number) {
+  const arr = [];
+
+  for (let i = 0; i < array.length; i += size) {
+    arr.push(array.slice(i, i + size));
+  }
+
+  return arr;
+}
+
+export { isLastIndex, chunk };
