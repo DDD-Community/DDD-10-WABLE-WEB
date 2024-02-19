61c0126622f0047a4adc9aaf3edd532762ec4a60
dokyung-kim <dkkim0122@gmail.com>
2024-02-19 22:53:40 +0900

    feat: 색상 관련 변수 export하여 styled components에서도 사용할 수 있도록 변경

---
+++ src/styles/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  black: '#242529',
+  white: '#ffffff',
+  point: '#ffe867',
+  waggle: {
+    gray: {
+      100: '#f8f9fa',
+      600: '#dcdcdd',
+      700: '#5c6873',
+      800: '#4d4f57',
+      900: '#36373f',
+    },
+  },
+};
+
+export default colors;
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,7 @@ import localFont from 'next/font/local';
 import { extendTheme } from '@chakra-ui/react';
 import { switchTheme } from './switch';
 import { dividerTheme } from './divider';
+import colors from './colors';
 
 const pretendard = localFont({
   src: '../assets/fonts/PretendardVariable.woff2',
@@ -13,18 +14,7 @@ const theme = extendTheme({
     heading: pretendard.style.fontFamily,
   },
   colors: {
-    black: '#242529',
-    white: '#ffffff',
-    point: '#ffe867',
-    waggle: {
-      gray: {
-        100: '#f8f9fa',
-        600: '#dcdcdd',
-        700: '#5c6873',
-        800: '#4d4f57',
-        900: '#36373f',
-      },
-    },
+    ...colors,
   },
   components: {
     Button: {
