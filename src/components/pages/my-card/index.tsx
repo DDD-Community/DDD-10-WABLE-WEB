d65722dd3fac06e6521dd651d8df04c5b566941e
dokyung-kim <dkkim0122@gmail.com>
2024-02-25 11:19:35 +0900

    feat: mock 데이터를 위한 파일 작성

--- src/components/pages/my-card/index.tsx
+++ src/components/pages/my-card/index.tsx
@@ -5,28 +5,10 @@ import { ProfileCardSection, CardListSection } from './styles';
 import GNBLayout from '@/layouts/gnb-layout';
 import ProfileCard from '@/components/pages/my-card/profile-card/index';
 import CardListHeader from './card-list/header';
+import { mockCardInfoForGrid, mockCardInfoForList, mockProfile } from './data';
 
 export default function MyCard() {
-  const profiles: ProfileInfo[] = [
-    {
-      name: '박경서',
-      nickname: '박경서_1',
-      gender: 'male',
-      birth: '1998-01-21',
-      profile_image_url: '~~~',
-      mbti: 'ENTP',
-      hobby: ['???', '??', '?', '직접입력'],
-    },
-    {
-      name: '김도경',
-      nickname: '김도경_1',
-      gender: 'female',
-      birth: '1996-01-21',
-      profile_image_url: '~~~',
-      mbti: 'ENTP',
-      hobby: ['???', '??', '?', '직접입력'],
-    },
-  ];
+  const profiles: ProfileInfo[] = mockProfile;
 
   const [openedProfileIndex, setOpenedProfileIndex] = useState(0);
   const [cardDirection, setCardDirection] = useState<CardDirection>('RECEIVED');
