30782db321ac8be5515defcff479b9e9abaffbe8
조성원 <tjddnjsjo@kakao.com>
2024-03-01 18:51:42 +0900

    fix: routes constants 수정

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,11 +1,13 @@
 export const ROUTES = {
-  HOME: (groupName: string) => `/${groupName}/home`,
+  HOME: '/',
   LOGIN: '/login',
   SIGN_UP: '/sign-up',
   GROUP: {
     CREATE: '/group/create',
+    HOME: (groupName: string) => `/${groupName}/home`,
+    MEMBER: (groupName: string) => `/${groupName}/member`,
     SETTING: (groupName: string) => `/${groupName}/setting`,
   },
   MY_CARD: '/my-card',
-  SETTING: '/settings',
+  SETTING: '/setting',
 };
