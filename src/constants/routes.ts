0336080a78382d77cefc761a492f696fd5dfdbff
조성원 <tjddnjsjo@kakao.com>
2024-03-01 17:39:10 +0900

    feat: my-card routes constant 추가

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,9 +1,10 @@
 export const ROUTES = {
-  HOME: '/',
+  HOME: (groupName: string) => `/${groupName}/home`,
   LOGIN: '/login',
   SIGN_UP: '/sign-up',
   GROUP: {
     CREATE: '/group/create',
     SETTING: (groupName: string) => `/group/${groupName}/setting`,
   },
+  MY_CARD: '/my-card',
 };
